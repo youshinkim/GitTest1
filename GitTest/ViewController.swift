2ac6eacc045a381095f8a7ac4bb2f94747420dfd
Holy <holy@iDreams-MacBook-Pro.local>
2017-11-06 21:10:07 -0800

    inserted button

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Call Git Test")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
